06d28dfb4359d9bd2a69aa848553da0f8b45c32c
exit <saraswathi.mac@gmail.com>
2023-09-29 19:07:37 +0530

    Updated the topbar

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,28 +1,10 @@
-import { signIn, signOut } from "next-auth/react";
 import React from "react";
-import Button from "~/components/common/Button";
-import useFetchSession from "~/hooks/useSession";
-import styles from './Home.module.scss'
+import TopBar from "../TopBar";
 
 const HomeComponent = () => {
-  let { session } = useFetchSession();
   return (
-    <div className={styles.authBtn}>
-      {session ? (
-        // <Button
-        //   onClick={() => signOut()}
-        //   btnClass="btn-primary"
-        //   title="Sign Out"
-        // />
-        <img className={styles.profileImg} src={session?.user.image as string} />
-      ) : (
-        <Button
-          onClick={() => signIn()}
-          btnClass="btn-primary"
-          title="Sign Up"
-        />
-        
-      )}
+    <div>
+      <TopBar />
     </div>
   );
 };
