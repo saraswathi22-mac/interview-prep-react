5edc01670c3ed5b700c7fd67e06da337129c799b
exit <saraswathi.mac@gmail.com>
2023-09-30 12:33:55 +0530

    Added Button in a folder

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Button } from "~/interface";
+import styles from "./Button.module.scss"
 
 const Button = ({ btnClass, title, onClick }: Button) => {
-  return <button onClick={onClick} className={`btn ${btnClass}`}>{title}</button>;
+  return <button onClick={onClick} className={`btn ${btnClass} ${styles.commonBtn}`}>{title}</button>;
 };
 
 export default Button;
