037610bca2fd425ae2acaeb51381dc0fbeeec1b5
exit <saraswathi.mac@gmail.com>
2023-10-04 10:10:05 +0530

    Updated showfiles in Home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TopBar from "../TopBar";
 import UploadFiles from "../UploadFiles";
+import ShowFiles from "../ShowFiles";
 
 const HomeComponent = () => {
   return (
     <div>
       <TopBar />
       <UploadFiles />
+      <ShowFiles />
     </div>
   );
 };
