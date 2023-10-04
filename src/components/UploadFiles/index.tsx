64702bce8503791e29c3a74e37e620a7de69bc35
exit <saraswathi.mac@gmail.com>
2023-10-04 12:11:32 +0530

    Updated upload files

--- src/components/UploadFiles/index.tsx
+++ src/components/UploadFiles/index.tsx
@@ -2,19 +2,35 @@ import React, { ChangeEvent, useState } from "react";
 import styles from "./Upload.module.scss";
 import Button from "../common/Button/Button";
 import { fileUpload } from "~/API/FileUpload";
+import { addFolder } from "~/API/Firestore";
 
 const UploadFiles = () => {
   const [isFileVisible, setFileVisible] = useState(false);
+  const [isFolderVisible, setFolderVisible] = useState(false);
+  const [folderName, setFolderName] = useState("");
 
   const uploadFile = async (event: ChangeEvent<HTMLInputElement>) => {
     let file = event.target.files?.[0];
     fileUpload(file);
   };
 
+  const uploadFolder = () => {
+    let payload = {
+      folderName: folderName,
+      isFolder: true,
+      fileList: [],
+    };
+    addFolder(payload);
+    setFolderName("");
+  };
+
   return (
     <div className={styles.uploadMain}>
       <Button
-        onClick={() => setFileVisible(!isFileVisible)}
+        onClick={() => {
+          setFileVisible(!isFileVisible);
+          setFolderVisible(false);
+        }}
         title="Add a File"
         btnClass="btn-primary"
       />
@@ -27,7 +43,32 @@ const UploadFiles = () => {
       ) : (
         <></>
       )}
-      <Button title="Add a Folder" btnClass="btn-success" />
+      <Button
+        onClick={() => {
+          setFileVisible(false);
+          setFolderVisible(!isFolderVisible);
+        }}
+        title="Add a Folder"
+        btnClass="btn-success"
+      />
+      {isFolderVisible ? (
+        <>
+          <input
+            type="text"
+            placeholder="Type here"
+            value={folderName}
+            onChange={(event) => setFolderName(event.target.value)}
+            className="input input-bordered input-accent w-full max-w-xs"
+          />
+          <Button
+            onClick={uploadFolder}
+            title="Create"
+            btnClass="btn-success"
+          />
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
