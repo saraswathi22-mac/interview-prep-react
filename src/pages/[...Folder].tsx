e93c413021bf5b51a11dfdf4b09adad70815dd37
exit <saraswathi.mac@gmail.com>
2023-10-06 23:05:35 +0530

    Updated folder files

--- src/pages/[...Folder].tsx
+++ src/pages/[...Folder].tsx
@@ -8,7 +8,8 @@ const Folder = () => {
   let parentId = router?.query?.id;
   return (
     <div>
-      <UploadFiles parentId={(parentId = "")} />
+      <UploadFiles parentId={parentId} />
+      <ShowFiles parentId={parentId} />
     </div>
   );
 };
