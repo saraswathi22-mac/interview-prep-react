701b50631dec513b552e78b0c55e05b805fc7a89
exit <saraswathi.mac@gmail.com>
2023-10-07 18:15:00 +0530

    Updated the session

--- src/pages/[...Folder].tsx
+++ src/pages/[...Folder].tsx
@@ -3,15 +3,24 @@ import React from "react";
 import ShowFiles from "~/components/ShowFiles";
 import TopBar from "~/components/TopBar";
 import UploadFiles from "~/components/UploadFiles";
+import useFetchSession from "~/hooks/useSession";
 
 const Folder = () => {
   const router = useRouter();
   let parentId = router?.query?.id;
+  let { session } = useFetchSession();
+
   return (
     <div>
       <TopBar />
-      <UploadFiles parentId={parentId as string} />
-      <ShowFiles parentId={parentId as string} />
+      {session ? (
+        <>
+          <UploadFiles parentId={parentId as string} />
+          <ShowFiles parentId={parentId as string} />
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
